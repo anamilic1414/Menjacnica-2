6f0733200578bf423b360cab9faec884445ca21a
anamilic1414 <ana.milic.pz@gmail.com>
2016-03-22 10:18:59 +0100

    Implementiran interfejs

--- Menjacnica/src/interfejs/Menjacnica.java
+++ Menjacnica/src/interfejs/Menjacnica.java
@@ -1,26 +1,45 @@
 package interfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import valute.Valuta;
 
 public class Menjacnica implements InterfejsMenjacnica {
+	
+	private LinkedList<Valuta> valute;
 
 	@Override
 	public void dodajValutu(String naziv, String skraceno, GregorianCalendar datum, String kurs, double iznos) {
-		// TODO Auto-generated method stub
+		Valuta nova = new Valuta();
+		nova.setDatum(datum);
+		nova.setKurs(kurs);
+		nova.setNaziv(naziv);
+		nova.setSkracenica(skraceno);
+		nova.setIznos(iznos);
+		
+		if(!valute.contains(nova)) 
+			valute.addLast(nova);
 
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, GregorianCalendar datum, String kurs) {
-		// TODO Auto-generated method stub
+	
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getDatum().equals(datum) && valute.get(i).getNaziv().equals(naziv) && valute.get(i).getKurs().equals(kurs))
+				valute.remove(valute.get(i));
+		}
 
 	}
 
 	@Override
 	public Valuta pronadjiValutu(String naziv, GregorianCalendar datum, String kurs) {
-		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getDatum().equals(datum) && valute.get(i).getKurs().equals(kurs) && valute.get(i).getNaziv().equals(naziv))
+				return valute.get(i);
+		}
 		return null;
 	}
 
