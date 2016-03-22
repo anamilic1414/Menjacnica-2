43c74583f7570f92b54d51024395e6289344236e
anamilic1414 <ana.milic.pz@gmail.com>
2016-03-22 08:57:46 +0100

    Dodate metode equals, hashCode i toString

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -50,4 +50,53 @@ public class Valuta {
 		this.iznos = iznos;
 	}
 
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skracenica=" + skracenica + ", datum=" + datum + ", kurs=" + kurs
+				+ ", iznos=" + iznos + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(iznos);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(iznos) != Double.doubleToLongBits(other.iznos))
+			return false;
+		if (kurs == null) {
+			if (other.kurs != null)
+				return false;
+		} else if (!kurs.equals(other.kurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		return true;
+	}
+
+	
 }
