f7c0662760f15e5aab8910f6ca7b4515120d0ced
anamilic1414 <ana.milic.pz@gmail.com>
2016-03-22 09:48:08 +0100

    Implementiran interfejs u klasi Menjacnica. Mala ispravka kod equals metode u klasi Valuta

--- Menjacnica/src/interfejs/Menjacnica.java
+++ Menjacnica/src/interfejs/Menjacnica.java
@@ -1,26 +1,51 @@
 package interfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import valute.Valuta;
 
 public class Menjacnica implements InterfejsMenjacnica {
 
+	private LinkedList<Valuta> valute;
+
 	@Override
 	public void dodajValutu(String naziv, String skraceno, GregorianCalendar datum, String kurs, double iznos) {
-		// TODO Auto-generated method stub
+		Valuta v = new Valuta();
+		v.setNaziv(naziv);
+		v.setSkracenica(skraceno);
+		v.setDatum(datum);
+		v.setKurs(kurs);
+		v.setIznos(iznos);
 
+		if (!valute.contains(v))
+			valute.add(v);
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, GregorianCalendar datum, String kurs) {
-		// TODO Auto-generated method stub
+		Valuta v = new Valuta();
+		v.setNaziv(naziv);
+		v.setDatum(datum);
+		v.setKurs(kurs);
+
+		if (valute.contains(v))
+			valute.remove(v);
 
 	}
 
 	@Override
 	public Valuta pronadjiValutu(String naziv, GregorianCalendar datum, String kurs) {
-		// TODO Auto-generated method stub
+		Valuta v = new Valuta();
+		v.setDatum(datum);
+		v.setKurs(naziv);
+		v.setNaziv(naziv);
+
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).equals(v))
+				return valute.get(i);
+		}
+
 		return null;
 	}
 
